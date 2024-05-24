87c31a257d8ae0cd2aa0d70ac5afd511db1c478a
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-24 10:14:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const submitBtn = document.querySelector("#btn");
+const textInput = document.querySelector("#text");
+const delayInput = document.querySelector("#delay");
+const textOutput = document.querySelector("#output");
+
+const wait = (delay)=>{
+	return new Promise((resolve,reject)=> {
+		setTimeout(()=> resolve(),delay);
+	})	
+}
+
+
+const handleInput = async ()=>{
+	textOutput.textContent = '';
+	const text = textInput.value;
+	const delay = +delayInput.value;
+	await wait(delay);
+	return new Promise((resolve,reject)=> resolve(text));	
+}
+
+submitBtn.addEventListener("click",()=>{
+	handleInput().then(msg=> textOutput.textContent = msg);	
+})
+
+
+
+
+
